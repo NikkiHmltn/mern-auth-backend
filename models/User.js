a910d87a3c166b4596e647c08e5cecb1f773ee3b
Nicole Hamilton <Nicole@NicolesLaptop.local>
2020-12-17 11:35:01 -0800

    [User] Add model and export model

---
+++ models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//User Schema
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        requried: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8,
+
+    },
+    date: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User;
